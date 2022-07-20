7a0b554b99d46cd4100666bb6e4fbc231025884b
Serg <vikingmasterserg@gmail.com>
2022-07-20 14:11:14 +0300

    refactor code

--- src/CoderCaesar/Menu.java
+++ src/CoderCaesar/Menu.java
@@ -6,12 +6,13 @@ public class Menu {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Выберите режим работы: \n" +
-                    "1 - зашифровать\n" +
-                    "2 - расшифровать\n" +
-                    "3 - применить метод перебора ключей\n" +
-                    "4 - применить метод синтаксического анализа\n" +
-                    "5 - выход из программы");
+            System.out.println("""
+                    Выберите режим работы:\s
+                    1 - зашифровать
+                    2 - расшифровать
+                    3 - применить метод перебора ключей
+                    4 - применить метод синтаксического анализа
+                    5 - выход из программы""");
             String string = scanner.nextLine();
             switch (string) {
                 case ("1") -> new Coder().coding();
